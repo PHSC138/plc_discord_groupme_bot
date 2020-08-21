d46a789204c70faa011ea6bc06b1b0b4fd1d2831
PHSC138 <PHSC138@users.noreply.github.com>
2020-08-20 23:04:50 -0400

    Can connect to server and channel

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const tokens = require('./token.js');  // Store tokens in token.js
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const discord_client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var discord_guild;
+var discord_channel;
+
+discord_client.on('ready', () => {
+  console.log(`Logged in as ${discord_client.user.tag}!`);
+  discord_guild = discord_client.guilds.fetch(tokens.discord_guild_id, () => {
+    discord_channel = discord_guild.channels.get(tokens.discord_channel_id);
+  });
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+// Forward all messages to groupme
+discord_client.on('message', msg => {
+  console.log(msg.content);
+  // Confirm that it's the guild and channel set in token.js
+  if (msg.guild.id !== tokens.discord_guild_id || msg.channel.id !== tokens.discord_channel_id) {
+    console.log("NOT PLC #groupme");
+    return;
   }
 });
 
-client.login(tokens.discord_token);
+discord_client.login(tokens.discord_token);
---
+++ example_token.js
@@ -0,0 +1,7 @@
+module.exports = {
+  discord_token: "bot_token",
+  discord_guild_id: "guild_id",
+  discord_channel_id: "channel_id",
+  groupme_token: "groupme_token",
+  gropme_bot_id: "groupme_bot_id"
+};
