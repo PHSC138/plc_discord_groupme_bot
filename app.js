dc0247e6f87acc55f6e41279f35ae0651d0801ef
PHSC138 <PHSC138@users.noreply.github.com>
2020-08-20 22:40:29 -0400

    Hello world (ping pong) working

---
+++ app.js
@@ -0,0 +1,15 @@
+const tokens = require('./token.js');  // Store tokens in token.js
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(tokens.discord_token);
