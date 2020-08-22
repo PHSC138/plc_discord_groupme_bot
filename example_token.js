2507291c4cfb669be3e8128636bc8c72556e1299
PHSC138 <PHSC138@users.noreply.github.com>
2020-08-22 11:45:55 -0400

    Updated example_token

--- example_token.js
+++ example_token.js
@@ -1,9 +1,12 @@
 module.exports = {
-  discord_token: "",          // Token for discord bot login
-  discord_guild_id: "",       // Server that bot is listening to
-  discord_channel_id: "",     // Channel bot should listen to
-  groupme_bot_id: "",         // ID of Groupme bot
-  groupme_callback_url: "",   // Callback path specified when Groupme bot created (in the form '/bot/')
-  groupme_avatar_url: "",     // Avatar url specified when Groupme bot created (in the form '/avatar/')
-  port: 5000                  // Port to run the bot on
+  discord_token: "",            // Token for discord bot login
+  discord_username: "",         // Discord bot username
+  discord_guild_id: "",         // Server that bot is listening to
+  discord_channel_id: "",       // Channel bot should listen to
+  discord_announcements_id: "", // Optional announcements channel
+  groupme_bot_id: "",           // ID of Groupme bot
+  groupme_group_id: "",         // ID of groupme group
+  groupme_callback_url: "",     // Callback path specified when Groupme bot created (in the form '/bot/')
+  groupme_avatar_url: "",       // Avatar url specified when Groupme bot created (in the form '/avatar/')
+  port: 5000                    // Port to run the bot on
 };
